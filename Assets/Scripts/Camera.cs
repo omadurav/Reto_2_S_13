11c0d968acc8522ff44da868e961d24fa825c6a5
omadurav <omadurav@outlook.com>
2023-08-14 22:24:50 -0500

    Configuración del movimiento
    Se configura el movimiento del player, y se crea la lógica para la cámara para que esta gire con el personaje.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Vector3 CamOffset = new Vector3(0f, 11f, -12f);
+
+    private Transform _target;
+
+    private void Start()
+    {
+        _target = GameObject.Find("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        //Establece la posición de la cámara
+        this.transform.position = _target.TransformPoint(CamOffset);
+
+        //El LookAt método actualiza la rotación de la cápsula en cada fotograma,
+        this.transform.LookAt(_target);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput _playerInput;
+    private Rigidbody _rbPlayer;
+
+    private float _rotationSpeed = 180f;
+    private Animator _animator;
+
+
+    private void Start()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _rbPlayer = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector2 movementInput = _playerInput.actions["Move"].ReadValue<Vector2>();
+        float moveH = movementInput.x * _rotationSpeed;
+        float moveV = movementInput.y;
+
+        //Movimiento y rotacion del player
+        MovePlayer(moveV);
+        Vector3 rotation = Vector3.up * moveH;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        _rbPlayer.MoveRotation(_rbPlayer.rotation * angleRot);
+    }
+
+    private void MovePlayer(float moveVertical)
+    {
+        float _moveSpeed = 4.5f;
+
+        if (moveVertical > 0)
+        {
+            _rbPlayer.MovePosition(this.transform.position + this.transform.forward * moveVertical * _moveSpeed * Time.fixedDeltaTime);
+            _animator.SetBool("isWalking", true);
+
+        }
+        else if (moveVertical < 0)
+        {
+            _moveSpeed = 2.6f;
+            _rbPlayer.MovePosition(this.transform.position + this.transform.forward * moveVertical * _moveSpeed * Time.fixedDeltaTime);
+            _animator.SetBool("isWalkingBack", true);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", false);
+            _animator.SetBool("isWalkingBack", false);
+        }
+    }
+}
